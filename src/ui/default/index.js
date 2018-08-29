bcb6fea490810a848af72fc64c1ad08e6ee955f7
Vahe <martinkenner@mail.com>
2018-08-29 16:20:20 +0400

    Renamed function names

--- src/ui/default/index.js
+++ src/ui/default/index.js
@@ -6,28 +6,28 @@ export function headerRenderer({ nextYear, next, prev, monthName, year }) {
   return (
     <React.Fragment>
       <div className="rad-ui-default--control-month">
-        <LeftArrow prev={prev} />
+        <LeftArrow onClick={prev} />
         <span>{monthName}</span>
-        <RightArrow next={next} />
+        <RightArrow onClick={next} />
       </div>
       <div className="rad-ui-default--control-year">
-        <LeftArrow prev={prev} />
+        <LeftArrow onClick={prev} />
         <span>{year}</span>
-        <RightArrow next={nextYear} />
+        <RightArrow onClick={nextYear} />
       </div>
     </React.Fragment>
   );
 }
 
-export function renderWeeks(value, index) {
+export function dayRenderer(value, index) {
   return (
-    <div key={index} className="rad-week rad-ui-default--week">
+    <div key={index} className="rad-day rad-ui-default--week">
       {value}
     </div>
   );
 }
 
-export function dayRenderer(value, index, iSelected, needHighlight, select) {
+export function dateRenderer(value, index, iSelected, needHighlight, select) {
   // index is the index
   // isSelected need for matching selected item
   // needHighlight is to matching next and previews weeks
@@ -35,7 +35,9 @@ export function dayRenderer(value, index, iSelected, needHighlight, select) {
   return (
     <div
       key={index}
-      onClick={select}
+      onClick={() => {
+        console.log(value);
+      }}
       className={cc({
         'rad__days--day': true,
         'rad__days--selected': iSelected,
